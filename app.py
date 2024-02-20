c883339c3d65e053ba3c0177d8084fc5baa841c6
A S <65203960+asathsara@users.noreply.github.com>
2024-02-20 22:14:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 print('Hello, World')
 first_num = int(input('Enter first num'))
 second_num = int(input('Enter Second num'))
+<<<<<<< HEAD
 print('Multiply of numbers are :-  ', first_num * second_num)
 print('Addition of numbers are :-  ', first_num + second_num)
+=======
+print('Multiply of numbers are :-  ' , first_num * second_num)
+print('Addition of numbers are :-  ' , first_num + second_num)
+>>>>>>> 13dfd2b (Update app.py)
