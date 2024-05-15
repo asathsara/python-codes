e5bc526c0f3a2febc5e8801a84c287f5b14f7ef3
A S <65203960+asathsara@users.noreply.github.com>
2024-05-15 20:46:07 +0530

    New Try

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 print('Hello, World')
 first_num = int(input('Enter first num'))
 second_num = int(input('Enter Second num'))
@@ -8,3 +9,10 @@ print('Addition of numbers are :-  ', first_num + second_num)
 print('Multiply of numbers are :-  ' , first_num * second_num)
 print('Addition of numbers are :-  ' , first_num + second_num)
 >>>>>>> 13dfd2b (Update app.py)
+=======
+first_num = float(input("Enter a num :-  "))
+second_num = float(input("Enter a second num :-  "))
+
+print(str(first_num) + ' + ' + str(second_num) + ' = ' + str(first_num + second_num)) # First method
+print(first_num,'+',second_num,'=',first_num + second_num) # Second method
+>>>>>>> 51d482c (New Try)
