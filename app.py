ee2a634c5d38040ffd25320dcc0bf34b668c2ba4
A S <65203960+asathsara@users.noreply.github.com>
2025-02-09 08:12:18 +0530

    git rebase problem fix

--- app.py
+++ app.py
@@ -1,79 +1,11 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-print('Hello, World')
-first_num = int(input('Enter first num'))
-second_num = int(input('Enter Second num'))
-<<<<<<< HEAD
-print('Multiply of numbers are :-  ', first_num * second_num)
-print('Addition of numbers are :-  ', first_num + second_num)
-=======
-print('Multiply of numbers are :-  ' , first_num * second_num)
-print('Addition of numbers are :-  ' , first_num + second_num)
->>>>>>> 13dfd2b (Update app.py)
-=======
-first_num = float(input("Enter a num :-  "))
-second_num = float(input("Enter a second num :-  "))
-
-print(str(first_num) + ' + ' + str(second_num) + ' = ' + str(first_num + second_num)) # First method
-print(first_num,'+',second_num,'=',first_num + second_num) # Second method
->>>>>>> 51d482c (New Try)
-=======
-# Python If else
-=======
-name = "Abcd"
->>>>>>> 91b8d14 (Strings)
-=======
-# Numbers
-from math import *
->>>>>>> e1813d6 (Numbers)
-=======
 # Word Replacing Game
->>>>>>> 844ce33 (Word Replacing Game)
 
 sentences = input('Enter your sentences: ')
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-# Python relies on indentation (whitespace at the beginning of a line) to define scope in the code. Other programming
-# languages often use curly-brackets for this purpose.
-if a > b:
-    print("a is greater than b")
-elif a == b:
-    print("a is equal to b")
-else:
-    print("b is greater than a")
->>>>>>> aac70d1 (If else and game)
-=======
-print(name.index('d'))
-print(name.replace('b','a'))
->>>>>>> 91b8d14 (Strings)
-=======
-numbers = [2,4,5,6]
-=======
 print(' ')
->>>>>>> 844ce33 (Word Replacing Game)
 
 word_one = input('Enter the word to replace: ')
 word_two = input('Enter the word to replace it with: ')
 
-<<<<<<< HEAD
-print(bin(1)) # Return the binary representation of an integer
-print('...........')
-print(sqrt(36))
->>>>>>> e1813d6 (Numbers)
-=======
 print('')
-<<<<<<< HEAD
-<<<<<<< HEAD
-print(sentences.replace(word_one,word_two)) # Replace word with another
->>>>>>> 844ce33 (Word Replacing Game)
-=======
-print(sentences.replace(word_one,word_two)) # Replace word with another
->>>>>>> 5dc3270 (MESURE SYSTEM)
-=======
 print(sentences.replace(word_one, word_two))  # Replace word with another
->>>>>>> 7e9c859 (Tuple Learn)
